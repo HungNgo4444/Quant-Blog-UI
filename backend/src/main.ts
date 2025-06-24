5b834fb506eba8e1a2f27301f5d9f7eb5da156c4
hoandk203 <hoanyttv@gmail.com>
2025-06-24 16:19:57 +0700

    main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -62,7 +62,7 @@ async function bootstrap() {
     },
   });
 
-  const port = process.env.PORT;
+  const port = process.env.PORT || 3001;
   await app.listen(port, () => {
     console.log(`🚀 Application is running on: http://localhost:${port}`);
     console.log(`📚 Swagger documentation: http://localhost:${port}/api/docs`);
